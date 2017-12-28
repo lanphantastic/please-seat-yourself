9d5e8e4a5025aca246d55da6449de86166b7de14
Lan Phan <lanc_phan@hotmail.com>
2017-12-28 09:49:57 -0500

    add description text to restaurant properly

--- db/migrate/20171228143542_create_add_imageto_restaurants.rb
+++
@@ -1,7 +0,0 @@
-class AddImagetoRestaurants < ActiveRecord::Migration[5.1]
-  def change
-    add_column :restaurants, :image, :string, null: false
-
-    add_column :restaurants, :twitter_handle, :string
-  end
-end
