6dca6842b6d1665034fadd32ba7cf214efaf76db
Lan Phan <lanc_phan@hotmail.com>
2017-12-28 10:46:10 -0500

    made new views for user and added data modelling relationship in the user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,14 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:alert] = "Please fix errors"
+      render :new
+    end
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :reservations
+  has_one :restaurant
+
+  validates :name, :email, :phone, presence: true
+  validates :email, uniqueness: true
+  validates :phone, format:{ with: /\A\d{10}\z/, message: 'must be in format 1234567878' }
+
+  def is_admin?
+    self.restaurant.present?
+  end
 end
