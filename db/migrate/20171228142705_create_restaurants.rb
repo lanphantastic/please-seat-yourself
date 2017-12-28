88d4c4779f1073c94cda1d378f0e947e007ac0e8
Lan Phan <lanc_phan@hotmail.com>
2017-12-28 09:30:57 -0500

    rails g model reservation with restaurant_id in the reservation model

--- db/migrate/20171228142705_create_restaurants.rb
+++ db/migrate/20171228142705_create_restaurants.rb
@@ -1,9 +1,14 @@
 class CreateRestaurants < ActiveRecord::Migration[5.1]
   def change
     create_table :restaurants do |t|
-      t.string :name
-      t.string :address
-      t.string :city
+      t.string :name, null: false
+      t.string :address, null: false
+      t.string :city, null: false
+      t.decimal :price_range, null: false
+      t.integer :total_seats, null: false
+      t.time :open_hour, null: false
+      t.time :close_hour, null: false
+      t.integer :user_id, null: false
 
       t.timestamps
     end
