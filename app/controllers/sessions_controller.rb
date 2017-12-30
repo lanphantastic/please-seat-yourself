d40e338eb69d1373195782f2c927c001f0e44409
Lan Phan <lanc_phan@hotmail.com>
2017-12-30 11:54:48 -0500

    creating the user session method and checking if both email and password are unique

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,27 @@ end
 
 def create
 
+  if params[:email].present?
+    if valid_email?(params[:email])
+      if params[:password].present?
+        user = User.find_by(email: params[:email])
+
+        # check if both email or password are correct
+        if user && user.authenticate( params[:password] )
+          session[:user_id] = user.id
+          redirect_to restaurants_url
+        else
+          flash[:notice] = "Wrong email/password combination"
+          render :new
+          flash.clear
+        end
+
+      # check password
+      end
+    # only one unique email
+    end
+    # email cannot be blank
+  end
 end
 
 def destroy
