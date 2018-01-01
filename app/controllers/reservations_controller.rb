3e472c3a582d92588b14fd5adcec8c20a8558fd2
Lan Phan <lanc_phan@hotmail.com>
2017-12-31 22:58:02 -0500

    confirmation method

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,13 @@
 class ReservationsController < ApplicationController
 
   def confirm
-    
+    @reservation = @restaurant.reservations.find(params[:id])
+    @reservation.status = "confirmed"
+    @reservation.save!
+    @reservation.send_confirmation_sms
+
+    flash[:notice] = "The reservation for #{@reservation.user.name} has been confirmed"
+    redirect_to admin_path
   end
 
   def cancel
